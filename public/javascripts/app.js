6740d78719de98e1c271b4396d2b21e126e03d02
SourenP <souren@papazian.info>
2014-02-16 16:02:43 -0500

    tried to fix javascript

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,8 +1,8 @@
-function on_load()
+function on_load() 
 {
     $('.mainMenuItem').click(function()
     {
-        var currMMI = $(this)
+        var currMMI = $(this);
         var currSML = getSubmenuListClass(currMMI);
         var currMLB = currMMI.find('.mainLineBox');
         var currMOLB = currMMI.find('.mainOpenLineBox');
@@ -10,7 +10,8 @@ function on_load()
         if(currMMI.hasClass("clicked"))
         {
             currMMI.removeClass("clicked");
-            if (currSML.length !== 0) {
+            if(currSML.length != 0)
+            {
                 currSML.slideUp(300);
                 currMLB.show();
                 currMOLB.hide();
@@ -26,18 +27,21 @@ function on_load()
 
                 if(currMMI.hasClass("clicked"))
                 {
-                    var currSML = getSubmenuListClass(currMMI);
+                    var currSML = getSubmenuListClass(currMMI)
                     currMMI.removeClass("clicked");
-                    if (currSML.length !== 0) {
+
+                    if(currSML.length != 0)
+                    {
                         currSML.slideUp(300);
-                        currMOLB.show();
-                        currMLB.hide();
+                        currMLB.show();
+                        currMOLB.hide();                       
                     }
                 }
             });
-
+            
             currMMI.addClass("clicked");
-            if (currSML.length !== 0) {
+            if(currSML.length != 0)
+            {
                 currMOLB.show();
                 currMLB.hide();
                 currSML.slideDown(300);
