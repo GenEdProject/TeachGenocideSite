5238e109af88cab5e8738ea1f075dc32b163a548
Souren Papazian <souren@papazian.info>
2014-09-07 15:08:38 -0400

    fixed typo

--- functions.php
+++ functions.php
@@ -99,7 +99,7 @@ function teachgen_widgets_init() {
   register_sidebar( array(
     'name'          => __( 'Sidebar', 'teachgen' ),
     'id'            => 'sidebar-1',
-    ',before_widget' => '<aside id="%1$s" class="widget %2$s">',
+    'before_widget' => '<aside id="%1$s" class="widget %2$s">',
     'after_widget'  => '</aside>',
     'before_title'  => '<h1 class="widget-title">',
     'after_title'   => '</h1>',
