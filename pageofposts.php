4eaad6eae5ae5bf95525038ed089b6e1c25f66e0
Souren Papazian <souren@papazian.info>
2014-09-12 17:06:28 -0400

    page type pageofposts used for home

--- pageofposts.php
+++ pageofposts.php
@@ -1,46 +1,51 @@
-<?php
+<?php 
 /*
-Template Name: Page Of Posts with Custom Fields
+Template Name: Page Of Posts
 */
 
 get_header(); ?>
-<?php
-if ( is_page() ) {
-    $category = get_post_meta( $posts[0]->ID, 'category', true );
-    $cat = get_cat_ID( $category );
-}
-if ( $cat ) :
-    $paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
-    $post_per_page = 4; // -1 shows all posts
-    $do_not_show_stickies = 1; // 0 to show stickies
-    $args=array (
-      'category__in' => array( $cat ),
-      'orderby' => 'date',
-      'order' => 'DESC',
-      'paged' => $paged,
-      'posts_per_page' => $post_per_page,
-      'ignore_sticky_posts' => $do_not_show_stickies
-    );
-    $temp = $wp_query; // assign original query to temp variable for later use  
-    global $wp_query;
-    $wp_query = null;
-    $wp_query = new WP_Query( $args ); 
-    if ( $wp_query->have_posts() ) : 
-        while ( $wp_query->have_posts() ) : $wp_query->the_post(); ?>
-        <div <?php post_class() ?> id="post-<?php the_ID(); ?>">
-            <div class="entry">
-            <?php the_content( 'Read the rest of this entry »' ); ?>
-            </div>
-        </div>
-        <?php endwhile; ?>
-        <?php endif; // if ( $wp_query->have_posts() ) ?>
-        <?php $wp_query = $temp; //reset back to original query ?>
 
-<?php else : ?>
-    <h2 class="center">Not Found</h2>
-    <p class="center">Sorry, but you are looking for something that isn't here.</p>
-    <?php get_search_form(); ?>    
-<?php endif; // if ( $cat ) ?>
+    <div id="primary" class="content-area">
+        <div id="content" class="site-content" role="main">
+        <?php 
+        /* The loop: the_post retrieves the content
+         * of the new Page you created to list the posts,
+         * e.g., an intro describing the posts shown listed on this Page..
+         */
+        if ( have_posts() ) :
+            while ( have_posts() ) : the_post();
 
-<?php get_sidebar(); ?>
+              // Display content of page
+              get_template_part( 'content', get_post_format() ); 
+              wp_reset_postdata();
+  
+            endwhile;
+        endif;
+
+        $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+
+        $args = array(
+            // Change these category SLUGS to suit your use.
+            'category_name' => 'press', 
+            'paged' => $paged
+        );
+
+        $list_of_posts = new WP_Query( $args );
+        ?>
+        <?php if ( $list_of_posts->have_posts() ) : ?>
+            <?php /* The loop */ ?>
+            <?php while ( $list_of_posts->have_posts() ) : $list_of_posts->the_post(); ?>
+                <div class="custom_post_container"> 
+                <div class="custom_post_title"> <?php the_title();?> </div>
+                <hr style="margin-bottom:10px; background-color:#000; height:1px; width:50% float:left">
+                <?php the_content();?>
+                </div>
+            <?php endwhile; ?>
+
+        <?php else : ?>
+        <?php endif; ?>
+
+        </div><!-- #content -->
+    </div><!-- #primary -->
+
 <?php get_footer(); ?>
