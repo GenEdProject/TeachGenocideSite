10a9856c082341edd7565b01d0e5fbfaee87987c
Souren Papazian <souren@papazian.info>
2014-09-26 01:54:37 -0400

    list categorizes by category (posts without category not displayed)

--- list.php
+++ list.php
@@ -13,29 +13,56 @@ $args = array(
 $myItems = new WP_Query( $args );
 $i = 0;
 $total = $myItems->found_posts;
+$category_array = array();
 ?>
 
-<?php ?>
-    <section class="list_item">
-    <div class="custom_page_padding">
+<?php while($i < $total) : $myItems->the_post(); ?>
     <?php
-    while($i < $total) : $myItems->the_post(); ?>
-      <section class="list_item_container">
-        <b><?php the_title() ; ?></b>
-        <p><?php
-               if (has_post_thumbnail()) {
-                   the_post_thumbnail();
-               }
-               the_content();?>
-        </p>
-      </section>
+        $category = get_the_category();
+        $category_name = (string) $category[0]->cat_name;
+        if(!in_array($category_name, $category_array) && ($category_name != ""))
+        {
+            $category_array[$category_name] = $category;
+            $j++;
+        }
+    ?>    
+<?php $i++; endwhile;?>
+
+
+
+
+<!-- For each category get posts -->
+
+
+<div class="custom_page_padding">
+<?php foreach ($category_array as $key => $cat) { ?>
+<section class="list_item">
     <?php
-      $i++;
-      endwhile;
+        $args = array(
+            'posts_per_page' => '-1',
+            'post_type' => $template_type,
+            'cat' => $cat[0]->cat_ID,);
+        $currCatItems = new WP_Query( $args );
+        $i = 0;
+        $total = $currCatItems->found_posts;
     ?>
-    </div>
-    </section>
-<?php ?>
+    <?php
+        while($i < $total) : $currCatItems->the_post(); ?>
+          <?php echo $key ?>
+          <section class="list_item_container">
+            <b><?php the_title(); ?></b>
+            <p><?php
+                   if (has_post_thumbnail()) {
+                       the_post_thumbnail();
+                   }
+                   the_content();?>
+            </p>
+          </section>
+    <?php $i++; endwhile;?>
+</section>
+<?php } ?>
+</div>
+
 
 <?php
 wp_reset_postdata();
