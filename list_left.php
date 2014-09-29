79186e3997e2034ac61753246ec6675c3152f8ad
Souren Papazian <souren@papazian.info>
2014-09-29 02:28:39 -0400

    made list_left for videos and books

---
+++ list_left.php
@@ -0,0 +1,109 @@
+<?php
+/*
+* Template Name: List Left
+* */
+
+get_header();
+
+$template_type = get_post_custom_values('template_type');
+$args = array(
+  'posts_per_page' => '-1',
+  'post_type' => $template_type,
+);
+$myItems = new WP_Query( $args );
+$i = 0;
+$total = $myItems->found_posts;
+$category_array = array();
+?>
+
+
+<!-- Get array of categories -->
+<?php while($i < $total) : $myItems->the_post(); ?>
+    <?php
+        if(has_category())
+        {
+            $category = get_the_category();
+            $category_name = (string) $category[0]->cat_name;
+            if(!in_array($category_name, $category_array))
+            {
+                $category_array[$category_name] = $category;
+            }
+        }
+    ?>    
+<?php $i++; endwhile;?>
+
+
+
+<?php function create_item() { ?>
+    <div class="item_container_left">
+        <b><?php the_title(); ?></b>
+        <p><?php
+               if (has_post_thumbnail()) {
+                   the_post_thumbnail();
+               }
+               the_content();
+        ?></p>
+    </div>
+<?php } ?>
+
+<?php function create_divider() { ?>
+    <hr class="item_divider_left">
+<?php } ?>
+
+
+<!-- For each category get posts -->
+<div class="custom_page_padding">
+<section class="list">
+<?php foreach ($category_array as $key => $cat) { // for each category?>
+    <?php
+        $args = array(
+            'posts_per_page' => '-1',
+            'post_type' => $template_type,
+            'cat' => $cat[0]->cat_ID,);
+        $currCatItems = new WP_Query( $args );
+        $i = 0;
+        $total = $currCatItems->found_posts;
+    ?>
+    <div class="title_container">
+        <center> <img src="<?php echo z_taxonomy_image_url($cat[0]->cat_ID); ?>" /> </center>
+        <h1> <?php echo $key ?> </h1>
+        <hr class="list_divider">
+    </div>
+
+
+    <div class="column" style="float: left">
+        <?php
+        while($i < ceil($total / 2)) : $currCatItems->the_post();
+            create_item();
+            if($i != ceil($total / 2) - 1)
+            {
+                create_divider();
+            }
+            $i++;
+        endwhile;
+        ?>
+    </div>
+
+    <div class="column" style="float: right">
+        <?php
+        while($i < $total) : $currCatItems->the_post();
+            create_item();
+            if($i != $total - 1)
+            {
+                create_divider();
+            }
+            $i++;
+        endwhile;
+        ?>
+    </div>
+    <div style="clear: both;"> </div>
+<?php } ?>
+</section>
+</div>
+
+
+<?php
+wp_reset_postdata();
+
+get_sidebar();
+get_footer(); ?>
