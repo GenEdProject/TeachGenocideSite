01a60ee030d2be30e068a171c5d9d7fe5370b7b3
Hayg Astourian <haygastourian@gmail.com>
2015-01-25 23:59:33 -0800

    Remove annoying 'menu' text
    This would appear when the screen shrank and the menu became togglable, for mobile

--- header.php
+++ header.php
@@ -42,7 +42,6 @@
     </div>
 
     <nav id="site-navigation" class="main-navigation" role="navigation">
-      <h1 class="menu-toggle"><?php _e( 'Menu', 'teachgen' ); ?></h1>
       <a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'teachgen' ); ?></a>
 
       <?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
