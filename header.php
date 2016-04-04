99ae572053e575a4b53eeffda08cb0fcb632c7a2
hayg.info <souren@papazian.info>
2016-04-04 14:58:01 -0700

    Switch font to load from https

--- header.php
+++ header.php
@@ -14,7 +14,7 @@
 <title><?php wp_title( '|', true, 'right' ); ?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
 <?php wp_enqueue_script('jquery'); ?>
 <?php wp_head(); ?>
 </head>
